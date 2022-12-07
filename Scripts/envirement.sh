b4e33970e65341b31f91d3ae5232aef94b00f3bc
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-12-07 06:48:57 -0300

    Update envirement.sh

--- Scripts/envirement.sh
+++ Scripts/envirement.sh
@@ -1,5 +1,6 @@
 
 DIR="/var/www/dataverse"
+DIRB = DIR + "/branding"
 
 echo $1
 
@@ -10,3 +11,11 @@ if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "Creating ${DIR}..."
 fi
+
+if [ -d "$DIRB" ]; then
+  # Take action if $DIR exists. #
+  echo "Creating ${DIRB}..."
+fi
+
+echo "## UPDATE LAYTOUTS ##"
+cp branding/* /var/www/dataverse/branding/ -R
